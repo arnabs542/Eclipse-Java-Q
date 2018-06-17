989ebeb36f765ae1e65f028f7518cc2e2a6997d1
bchen22@syr.edu <bchen22@syr.edu>
2018-06-16 23:57:22 -0400

    bipartite

--- src/bfsRelated/Bipartite.java
+++ src/bfsRelated/Bipartite.java
@@ -20,7 +20,10 @@
 package bfsRelated;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class GraphNode {
 	public int key;
@@ -32,13 +35,38 @@ class GraphNode {
     }
 }
 
-
 public class Bipartite {
 	
 	public boolean isBipartite(List<GraphNode> graph) {
-		
-		
+		HashMap<GraphNode, Integer> visited = new HashMap<GraphNode, Integer>();
+		for (GraphNode node : graph) {
+			if(!BFS(node, visited)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
-
+	private boolean BFS(GraphNode node, HashMap<GraphNode, Integer> visited) {
+		if (visited.containsKey(node)) {
+			return true;
+		}
+		Queue<GraphNode> queue = new LinkedList<GraphNode>();
+		queue.offer(node);
+		visited.put(node, 0);
+		while (!queue.isEmpty()) {
+			GraphNode cur = queue.poll();
+			int curGroup = visited.get(cur);
+			int neiGroup = curGroup == 0 ? 1 : 0;
+			for (GraphNode nei : cur.neighbors) {
+				if (!visited.containsKey(nei)) {
+					visited.put(nei, neiGroup);
+					queue.offer(nei);
+				} else if (visited.get(nei) != neiGroup) {
+					return false;
+				}			
+			}
+		}
+		return true;
+	}
 }
