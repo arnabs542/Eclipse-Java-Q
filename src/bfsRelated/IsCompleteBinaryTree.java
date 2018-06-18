ba397ef518363bdd486d3c1cd50699757d1ee6d1
bchen22@syr.edu <bchen22@syr.edu>
2018-06-18 00:50:56 -0400

    IsCompleteBinaryTree.java

--- src/bfsRelated/IsCompleteBinaryTree.java
+++ src/bfsRelated/IsCompleteBinaryTree.java
@@ -7,18 +7,48 @@
  *     except possibly the last level.
  *   Furthermore, all nodes are as far left as possible.
  *   
- *   
- * Updated:
+ *  Corner Cases:
+ *    What if the binary tree is null? Return true in this case.
+ *     
  * 
  */
 
 package bfsRelated;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import binaryTreeRelated.TreeNode;
 
 public class IsCompleteBinaryTree {
 	
 	public boolean checkCompleted(TreeNode root) {
-		return false;
+		if (root == null) { // corner case
+			return true;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		boolean flag = false; // if flag is set true, we should not see any node with child
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			TreeNode cur = queue.poll(); // expand a node from queue
+			
+			if (cur.left == null) { // if a node has only one child, set the flag to true
+				flag = true;
+			} else if (flag) { // if the flag is set, and we still see a node with child
+				return false;
+			} else { // the flag is not set and the left child present, generate its left child
+				queue.offer(cur.left);
+			}
+			
+			// same logic applied to the right child
+			if (cur.right == null) {
+				flag = true;
+			} else if (flag) {
+				return false;
+			} else {
+				queue.offer(cur.right);
+			}
+		}
+		return true;
 	}
 
 }
