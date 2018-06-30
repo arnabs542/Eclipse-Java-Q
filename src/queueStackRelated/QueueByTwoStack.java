4c35665842375f58d3790f9f07917ab6efc5d0fe
bchen22@syr.edu <bchen22@syr.edu>
2018-06-30 09:50:46 +0800

    review

--- src/queueStackRelated/QueueByTwoStack.java
+++ src/queueStackRelated/QueueByTwoStack.java
@@ -1,11 +1,14 @@
 /*
  * Created Date: May 15,2018
- * Question: Queue By Two Stacks
- * Implement a queue by using two stacks. The queue should provide size(), isEmpty(), offer(), poll() and peek() operations. 
- * When the queue is empty, poll() and peek() should return null.
  * 
- * Updated: June 1, 2018
- * Time Complexity Analysis
+ * Question-  Queue By Two Stacks
+ *   Implement a queue by using two stacks. The queue should provide size(), isEmpty(), offer(), poll() and peek() operations. 
+ *   When the queue is empty, poll() and peek() should return null.
+ * 
+ * Updated: 
+ *   June 1, 2018: Time Complexity Analysis
+ *   June 30, 2018: Review
+ * 
  */
 
 package queueStackRelated;
@@ -15,15 +18,20 @@ import java.util.LinkedList;
 
 public class QueueByTwoStack {
 		
-	Deque<Integer> inStack = new LinkedList<>();
-	Deque<Integer> outStack = new LinkedList<>();
+	Deque<Integer> inStack; // always insert elements to inStack
+	Deque<Integer> outStack; // always poll elements from outStack
+	
+	public QueueByTwoStack () {
+		inStack = new LinkedList<>();
+		outStack = new LinkedList<>();
+	}
 		
 	public Integer size() {
 		return inStack.size() + outStack.size();
 	}
 	
 	public boolean isEmpty() {
-		if(inStack.isEmpty() && outStack.isEmpty()) {
+		if (inStack.isEmpty() && outStack.isEmpty()) {
 			return true;
 		} else {
 			return false;
@@ -36,9 +44,9 @@ public class QueueByTwoStack {
 	
 	// Time Complexity
 	// ## worst case, when outStack is empty, do the following:
-	// (1) pop all the elements of inStack	O(n)
-	// (2) push all the elements in outStack O(n)
-	// (3) call outStack.pop					O(1)
+	// (1) pop all the elements of inStack --- O(n)
+	// (2) push all the elements in outStack --- O(n)
+	// (3) call outStack.pop --- O(1)
 	// 	thus, worst case -> O(n)
 	//
 	// ## amortized time
@@ -53,54 +61,54 @@ public class QueueByTwoStack {
 	// Space Complexity: O(1);
 	
 	public Integer poll() {		
-		if(outStack.isEmpty()) {
-			if(inStack.isEmpty()) {
-				return null;
-			}
-			else {
-				while(inStack.isEmpty() == false) {
-					outStack.push(inStack.pop());
-				}
-			}
-		}
-		return outStack.pop();
+		move();
+		return outStack.isEmpty() ? null : outStack.pop(); 
 	}
 	
 	public Integer peek() {	
-		if(outStack.isEmpty()) {
-			if(inStack.isEmpty()) {
-				return null;
-			}
-			else {
-				while(inStack.isEmpty() == false) {
-					outStack.push(inStack.pop());
-				}
+		move();
+		return outStack.isEmpty()? null : outStack.peek();
+	}
+	
+	private void move() {
+		if (outStack.isEmpty()) {
+			while (!inStack.isEmpty()) {
+				outStack.push(inStack.pop());
 			}
 		}
-		return outStack.peek();
 	}
 
-	public static void main(String[] args) {	
+	/* ----------------------< test stub >-------------------------*/
+	public static void main(String[] args) {
 		
 		QueueByTwoStack myQ = new QueueByTwoStack();
-		System.out.println(myQ.isEmpty());
-		System.out.println("size = " + myQ.size());
-		System.out.println("peek " + myQ.peek());
 		
+		/* Test Case 0 */
+		System.out.println("---< Test Case 0 >---");
+		System.out.println(myQ.isEmpty()); // expected: true
+		System.out.println("size = " + myQ.size()); // expected: 0
+		System.out.println("peek " + myQ.peek()); // expected: null
+		
+		/* Test Case 1 */
+		System.out.println("\n---< Test Case 1 >---");
 		myQ.offer(1);
 		myQ.offer(2);
-		System.out.println("peek " + myQ.peek());
+		System.out.println("peek " + myQ.peek()); // expected: 1
 		myQ.offer(3);
-		System.out.println("peek " + myQ.peek());
+		System.out.println("peek " + myQ.peek()); // expected: 1
 		
-		System.out.println(myQ.isEmpty());
-		System.out.println("peek " + myQ.peek());
-		System.out.println("size = " + myQ.size());
-		System.out.println("poll: " + myQ.poll());
-		System.out.println("size = " + myQ.size());
-		System.out.println("peek " + myQ.peek());
-		System.out.println("poll: " + myQ.poll());
-		System.out.println("poll: " + myQ.poll());
+		/* Test Case 2 */
+		System.out.println("\n---< Test Case 2 >---");
+		System.out.println(myQ.isEmpty()); // expected: false
+		System.out.println("size = " + myQ.size()); // expected: 3
+		System.out.println("poll: " + myQ.poll()); // expected: 1
+		System.out.println("size = " + myQ.size()); // expected: 2
+		System.out.println("peek " + myQ.peek()); // expected: 2
+		System.out.println("poll: " + myQ.poll()); // expected: 2
+		System.out.println("poll: " + myQ.poll()); // expected: 3
+		
+		/* Test Case 3 */
+		System.out.println("\n---< Test Case 3 >---");	
 	}
 }
 
