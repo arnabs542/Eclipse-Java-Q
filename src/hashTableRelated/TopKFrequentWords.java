ebfa4447964c2bc36dc4a02407bc19dcb9951d3a
bchen22@syr.edu <bchen22@syr.edu>
2018-09-28 02:10:58 -0400

    update

--- src/hashTableRelated/TopKFrequentWords.java
+++ src/hashTableRelated/TopKFrequentWords.java
@@ -1,17 +1,19 @@
 /*
- * Created Date: June 26, 2018
+ * == Created Date ==
+ * June 26, 2018
  * 
- * Question - Top K Frequent Words:
- *   Given a composition with different kinds of words, return a list of the top K most frequent words in the composition.
- *   ordered from most frequent one to least frequent one (the list could be of size K or smaller than K)
+ * == Question - Top K Frequent Words ==
+ * Given a composition with different kinds of words, return a list of the top K most frequent words in the composition.
+ * ordered from most frequent one to least frequent one (the list could be of size K or smaller than K)
  *   
- *   Example: 
- *     Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 2 frequent words are [“b”, “c”]
- *     Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 4 frequent words are [“b”, “c”, "a", "d"]
- *     Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 5 frequent words are [“b”, “c”, "a", "d"]
+ * == Example ==
+ * Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 2 frequent words are [“b”, “c”]
+ * Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 4 frequent words are [“b”, “c”, "a", "d"]
+ * Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 5 frequent words are [“b”, “c”, "a", "d"]
  *   
- * Updated:
- *   June 27, 2018: Review
+ * == Updated ==
+ * June 27, 2018: Review
+ * Sept. 28, 2018: Review
  * 
  */
 
@@ -19,6 +21,8 @@ package hashTableRelated;
 
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -27,7 +31,7 @@ public class TopKFrequentWords {
 		if (combo.length == 0) {
 			return new String[0];
 		}
-		
+		// Step1: use a hash table to record the value and frequency 
 		Map<String, Integer> freqMap = new HashMap<>();
 		for (String s : combo) {
 			if (!freqMap.containsKey(s)) {
@@ -38,16 +42,17 @@ public class TopKFrequentWords {
 			}
 		}
 		
+		// Step 2: create a min Heap
 		class MapComparator implements Comparator<Map.Entry<String, Integer>> {
 			@Override
 			public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
 				return e1.getValue().compareTo(e2.getValue());
 			}
-		}
-		
+		}		
 		// line below has to be under class MapComparator
 		PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<>(k, new MapComparator());
 		
+	    // Step 3, iterate the frequency map, get the top frequency elements in the heap
 		for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
 			if (minHeap.size() < k) {
 				minHeap.offer(entry);
