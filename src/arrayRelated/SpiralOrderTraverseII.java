2f745d9f44d35f7936582344606bd21fe5e11367
bchen22@syr.edu <bchen22@syr.edu>
2018-10-10 22:08:23 -0400

    update

--- src/arrayRelated/SpiralOrderTraverseII.java
+++ src/arrayRelated/SpiralOrderTraverseII.java
@@ -1,15 +1,22 @@
 /*
- * Created Date: August 4, 2018
+ * == Created Date ==
+ * August 4, 2018
  * 
- * Question - Spiral Order Traverse II:
- *   Traverse an M  N 2D array in spiral order clock-wise starting from the top left corner. 
- *   Return the list of traversal sequence.
+ * == Question - Spiral Order Traverse II ==
+ * Traverse an M  N 2D array in spiral order clock-wise starting from the top left corner. 
+ * Return the list of traversal sequence.
  *    
- *   Example: 
- *     { {1,  2,  3,  4},
- *       {5,  6,  7,  8},
- *       {9, 10, 11, 12} }
- *     the traversal sequence is [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+ * == Example ==
+ * { 
+ *   {1,  2,  3,  4},
+ *   {5,  6,  7,  8},
+ *   {9, 10, 11, 12} 
+ * }
+ * 
+ * The traversal sequence is [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+ * 
+ * == Note ==
+ * October 10, 2018: Add iterative solution 
  * 
  */
 
@@ -20,7 +27,9 @@ import java.util.List;
 
 public class SpiralOrderTraverseII {
 	
-	public List<Integer> spiral(int[][] matrix) {
+	/* ------------------------< Solution - Recuriosn >----------------- */ 
+	
+	public List<Integer> spiralMeth1(int[][] matrix) {
 		List<Integer> result = new ArrayList<>();
 		helper(matrix, matrix.length, matrix[0].length, 0, result);
 		return result;
@@ -69,60 +78,118 @@ public class SpiralOrderTraverseII {
 		helper(matrix, rowRem - 2, colRem - 2, offset + 1, res);
 	}
 	
+	/* ------------------------< Solution - Iteration >----------------- 
+	 *
+	 * {  left         right
+	 *     {1,  2,  3,  4},  up
+	 *     {5,  6,  7,  8},
+	 *     {9, 10, 11, 12}   down
+	 * }
+	 * 
+	 */
+    public List<Integer> spiralMeth2(int[][] matrix) {
+    		List<Integer> result = new ArrayList<>(); 
+    		if (matrix.length == 0) {
+    			return result;
+    		}
+    		
+    		int left = 0;
+    		int right = matrix[0].length - 1;
+    		int up = 0;
+    		int down = matrix.length - 1;
+
+        while (left <= right && up <= down) {
+            for (int col = left; col <= right; col++) { // up row
+            		result.add(matrix[up][col]);
+            }
+            for (int row = up + 1; row <= down; row++) { // right col
+        			result.add(matrix[row][right]);
+            }
+            
+            if (up < down && left < right) {
+                for (int col = right - 1; col > left; col--) { // down row
+            			result.add(matrix[down][col]);
+                }
+
+                for (int row = down; row > up; row--) { // left col
+            			result.add(matrix[row][left]);
+                }            
+            }
+            up++;
+            right--;
+            down--;
+            left++;
+        }
+        return result;
+    }
+	
+	
 	/* ----------------------< test stub >-------------------------*/
+	private static void print(List<Integer> list) {
+		for (int item : list) {
+			System.out.print(item + " ");
+		} 
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
 		
 		SpiralOrderTraverseII testObj = new SpiralOrderTraverseII();
 		
-		/* Test Case 0 */
+		/* -----------------------< Test Case 0 >--------------------------------*/
 		System.out.println("---< Test Case 0 >---");
 		
 		int[][] m0 = {{1},{2},{3}};	
-		List<Integer> l0 = testObj.spiral(m0);
 		
-		for(int item : l0) {
-			System.out.print(item + " ");
-		} // expected: 1, 2, 3
+		List<Integer> result = testObj.spiralMeth1(m0);	
+		print(result); // expected: 1, 2, 3
 		
-		/* Test Case 1 */
+		result = testObj.spiralMeth2(m0);	
+		print(result); // expected: 1, 2, 3
+		
+		/* -----------------------< Test Case 1 >--------------------------------*/
 		System.out.println("\n---< Test Case 1 >---");
 		
-		int[][] m1 = {{1,2,3}, {4,5,6}, {7,8,9}};
-		List<Integer> l1 = testObj.spiral(m1);
+		int[][] m1 = {
+				      {1,2,3}, 
+				      {4,5,6}, 
+				      {7,8,9}
+				     };
 		
-		for(int item : l1) {
-			System.out.print(item + " ");
-		} // expected: 1, 2, 3, 6, 9, 8, 7, 4, 5
+		result = testObj.spiralMeth1(m1);
+		print(result); // expected: 1, 2, 3, 6, 9, 8, 7, 4, 5
 		
-		/* Test Case 2 */
+		result = testObj.spiralMeth2(m1);
+		print(result); // expected: 1, 2, 3, 6, 9, 8, 7, 4, 5
+		
+		/* -----------------------< Test Case 2 >--------------------------------*/
 		System.out.println("\n---< Test Case 2 >---");
 		
-		int[][] m2 = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};		
-		List<Integer> l2 = testObj.spiral(m2);
+		int[][] m2 = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};	
 		
-		for(int item : l2) {
-			System.out.print(item + " ");
-		} // expected: 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7
+		result = testObj.spiralMeth1(m2);	
+		print(result);// expected: 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7
 		
-		/* Test Case 3 */
+		result = testObj.spiralMeth2(m2);	
+		print(result);// expected: 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7
+		
+		/* -----------------------< Test Case 3 >--------------------------------*/
 		System.out.println("\n---< Test Case 3 >---");
 		
 		int[][] m3 = {{1,2,3,4}};		
-		List<Integer> l3 = testObj.spiral(m3);
+		result = testObj.spiralMeth1(m3);		
+		print(result); // expected: 1, 2, 3, 4
 		
-		for(int item : l3) {
-			System.out.print(item + " ");
-		} // expected: 1, 2, 3, 4
+		result = testObj.spiralMeth2(m3);		
+		print(result); // expected: 1, 2, 3, 4
 		
-		/* Test Case 4 */
+		/* -----------------------< Test Case 4 >--------------------------------*/
 		System.out.println("\n---< Test Case 4 >---");
 		
 		int[][] m4 = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};		
-		List<Integer> l4 = testObj.spiral(m4);
+		List<Integer> l4 = testObj.spiralMeth1(m4);
 		
-		for(int item : l4) {
-			System.out.print(item + " ");
-		} // expected: 1, 2, 3, 4, 5, 10, 15, 14, 13, 12, 11, 6, 7, 8, 9
+		print(l4);// expected: 1, 2, 3, 4, 5, 10, 15, 14, 13, 12, 11, 6, 7, 8, 9
 		
 	}
 
--- src/dfsRelated/WordSearch.java
+++ src/dfsRelated/WordSearch.java
@@ -2,7 +2,7 @@ package dfsRelated;
 
 public class WordSearch {
 	
-	/* ------------------------ < Wrong Approach !!! >--------------------------- */
+	/* ------------------------ < Wrong Approach, should use DFS to solve this problem !!! >--------------------------- */
     public boolean existWrong(char[][] board, String word) {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[0].length; col++) {
@@ -43,8 +43,15 @@ public class WordSearch {
     }
     
    
-    /* ------------------------ < DFS !!! >--------------------------- */
-    public boolean exist(char[][] board, String word) {
+    /* ------------------------ < Solution 1 - DFS with boolean[][] visited >--------------------------- 
+     * TC: O(4 ^ N * M) 
+     * SC: O(M) 
+     * 
+     * M: number of elements in the board
+     * N: number of characters of the given word 
+     *  
+     */
+    public boolean existMeth1(char[][] board, String word) {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[0].length; col++) {
                 boolean[][] visited = new boolean[board.length][board[0].length];
@@ -57,16 +64,68 @@ public class WordSearch {
     }
     
     private boolean dfs(char[][] board, int row, int col, String word, int index, boolean[][] visited) {
-            
         
+    		// check if the current position is valid in board
         if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
             return false;
         }
         
+        // check if current element of board has been visited before
         if (visited[row][col]) {
             return false;
         }
         
+        // check if current element of board matched the corresponding character of the given word
+        if (board[row][col] != word.charAt(index)) {
+            return false;           
+        } 
+        
+        // check if this the last character of the given word
+        if (index + 1 == word.length()) {
+            return true;
+        }
+        
+        // mark this element as visited
+        visited[row][col] = true;
+          
+        // recursively DFS the four direction of the current element
+        boolean result =	 dfs(board, row + 1, col, word, index + 1, visited) || 
+        					 dfs(board, row - 1, col, word, index + 1, visited) ||
+        					 dfs(board, row, col + 1, word, index + 1, visited) || 
+        					 dfs(board, row, col - 1, word, index + 1, visited);
+        
+        // extremely important!!! restore the visited state for backtracking
+        visited[row][col] = false;
+        
+        return result;
+    }
+    
+    
+    /* ------------------------ < Solution 2 - DFS without boolean[][] visited >---------------------------
+     * TC: O(4 ^ N * M) 
+     * SC: O(1)
+     * 
+     * M: number of elements in the board
+     * N: number of characters of the given word 
+     *  
+     */
+    public boolean existMeth2(char[][] board, String word) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (dfs(board, row, col, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int row, int col, String word, int index) {
+            
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+            return false;
+        }
+
         if (board[row][col] != word.charAt(index)) {
             return false;           
         } 
@@ -75,14 +134,17 @@ public class WordSearch {
             return true;
         }
         
-        visited[row][col] = true;
-          
-        boolean result =	 dfs(board, row + 1, col, word, index + 1, visited) || 
-        					 dfs(board, row - 1, col, word, index + 1, visited) ||
-        					 dfs(board, row, col + 1, word, index + 1, visited) || 
-        					 dfs(board, row, col - 1, word, index + 1, visited);
-        visited[row][col] = false;
-        return result;
+        char temp = board[row][col];
+        board[row][col] = '#'; // mark as visted before
+        
+        boolean existed = dfs(board, row + 1, col, word, index + 1) || 
+                          dfs(board, row - 1, col, word, index + 1) ||
+                          dfs(board, row, col + 1, word, index + 1) || 
+                          dfs(board, row, col - 1, word, index + 1);
+        
+        board[row][col] = temp; // restore the element
+        
+        return existed;
     }
     
 	/* ----------------------< test stub >-------------------------*/
@@ -102,7 +164,8 @@ public class WordSearch {
 			                           
 		System.out.println(testObj.existWrong(board1, "acdb")); // expected: true 
 		
-		System.out.println(testObj.exist(board1, "acdb")); // expected: true 
+		System.out.println(testObj.existMeth1(board1, "acdb")); // expected: true 
+		System.out.println(testObj.existMeth2(board1, "acdb")); // expected: true 
 		
 		/* Test Case 2 */
 		System.out.println("---< Test Case 2 >---");
@@ -114,7 +177,8 @@ public class WordSearch {
 									   
 		System.out.println(testObj.existWrong(board2, "AAB")); // expected: true, BUT 'false' !!!
 		
-		System.out.println(testObj.exist(board2, "AAB")); // expected: true 
+		System.out.println(testObj.existMeth1(board2, "AAB")); // expected: true 
+		System.out.println(testObj.existMeth2(board2, "AAB")); // expected: true 
 		
 		/* Test Case 3 */
 		System.out.println("---< Test Case 3 >---");
@@ -125,7 +189,8 @@ public class WordSearch {
 			   						    new char[] {'A', 'D', 'E', 'E'},
 									  };
 									  
-		System.out.println(testObj.exist(board3, "ABCESEEEFS")); // expected: true 
+		System.out.println(testObj.existMeth1(board3, "ABCESEEEFS")); // expected: true 
+		System.out.println(testObj.existMeth2(board3, "ABCESEEEFS")); // expected: true 
 		
 	}
 
