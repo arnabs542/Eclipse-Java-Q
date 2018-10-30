b4ef3a905859f0ed22a61a6ea9557650547d6bfa
bchen22@syr.edu <bchen22@syr.edu>
2018-10-30 17:18:16 -0400

    update

--- src/arrayRelated/DetermineIfOneStringIsAnothersSubstring.java
+++ src/arrayRelated/DetermineIfOneStringIsAnothersSubstring.java
@@ -70,19 +70,45 @@ public class DetermineIfOneStringIsAnothersSubstring {
 		if (small.length() == 0) {
 			return 0;
 		}
-//		
-//		int largePrime = 101;
-//		int prime = 31;
-//		int seed = 1;
-//		
-//		int targetHash = small.charAt(0) % largePrime;
-//		
-//		int hash = 0;
-//		for (int i = 0; i < small.length(); i++) {
-//			hash = moduleHash(hash, large.charAt(i), prime, largePrime);
-//		}
-		// incomplete
-		return 0;
+		
+		int largePrime = 101;
+		int prime = 31;
+		int seed = 1;
+		
+		int targetHash = small.charAt(0) % largePrime;		
+		for (int i = 1; i < small.length(); i++) {
+			seed = moduleHash(seed, 0, prime, largePrime);
+			targetHash = moduleHash(targetHash, small.charAt(i), prime, largePrime);
+		}
+		
+		int hash = 0;
+		for (int i = 1; i < small.length(); i++) {
+			hash = moduleHash(hash, large.charAt(i), prime, largePrime);
+		}
+		
+		if (hash == targetHash && equals(large, 0, small)) {
+			return 0;
+		}
+		
+		for (int i = 1; i <= large.length() - small.length(); i++) {
+			hash = nonNegative(hash - seed * large.charAt(i - 1) % largePrime, largePrime);
+			hash = moduleHash(hash,  large.charAt(i + small.length() - 1), prime, largePrime);
+			if (hash == targetHash && equals(large, i, small)) {
+				return i;
+			}
+		}		
+		return -1;
+	}
+	
+	private int moduleHash(int hash, int addition, int prime, int largePrime) {
+		return (hash * prime % largePrime + addition) % largePrime;
+	}
+	
+	private int nonNegative(int hash, int largePrime) {
+		if (hash < 0) {
+			hash += largePrime;
+		}
+		return hash;
 	}
 	
 	// Time Complexity: O(?);
@@ -99,6 +125,7 @@ public class DetermineIfOneStringIsAnothersSubstring {
 		/* Test Case 1 */
 		System.out.println("---< Test Case 1 >---");
 		System.out.println(testObj.strStrI("abcdedg", "dg"));
+		System.out.println(testObj.strstr("abcdedg", "dg"));
 		
 		/* Test Case 2 */
 		System.out.println("---< Test Case 2 >---");
