70c734c029cf07cadd46739bf91e526d9af938d7
bchen22@syr.edu <bchen22@syr.edu>
2019-01-02 04:51:27 -0500

    2019.1.1-practice

--- src/dfsRelated/CheapestFlightsWithinKStops.java
+++ src/dfsRelated/CheapestFlightsWithinKStops.java
@@ -19,12 +19,12 @@
 package dfsRelated;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class CheapestFlightsWithinKStops {
 	
@@ -51,14 +51,6 @@ public class CheapestFlightsWithinKStops {
 		return minCost == Integer.MAX_VALUE ? -1 : minCost;
 	}
 	
-	private void printResult() {
-		System.out.println("cost = " + minCost);
-		for (int city : finalPath) {
-			System.out.print(city + " ");
-		}
-		System.out.println();
-	}
-	
 	private void dfs(int from, int dst, int stops, int K, int cost, List<Integer> path) {
 		if (stops > K || cost > minCost) {
 			return;
@@ -78,6 +70,14 @@ public class CheapestFlightsWithinKStops {
 		}
 	}
 	
+	private void printResult() {
+		System.out.println("cost = " + minCost);
+		for (int city : finalPath) {
+			System.out.print(city + " ");
+		}
+		System.out.println();
+	}
+	
 	private List<Map<Integer,Integer>> getCityGraph(int n, int[][] flights) {
 		List<Map<Integer,Integer>> graph = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
@@ -90,39 +90,95 @@ public class CheapestFlightsWithinKStops {
 		return graph;
 	}
 	
-    /* ----------------------< Method 2 : Dijkstra Algorithm >-------------------------
+    /* ----------------------< Method 2 : Modified Dijkstra Algorithm >-------------------------
      * 
      * Time Complexity: O(?);
      * Space Complexity: O(?);	
      * 
      * */
-	
-    public int findCheapestPriceII(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, Map<Integer, Integer>> prices = new HashMap<>();
-        for (int[] f : flights) {
-            if (!prices.containsKey(f[0])) prices.put(f[0], new HashMap<>());
-            prices.get(f[0]).put(f[1], f[2]);
+    private class City implements Comparable<City> {
+        private int id;
+        private int costFromSrc;
+        private int stopFromSrc;
+        private int from;
+        public City(int id, int costFromSrc, int stopFromSrc, int from) {
+            this.id = id;
+            this.costFromSrc = costFromSrc;
+            this.stopFromSrc = stopFromSrc;
+            this.from = from;
         }
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> (Integer.compare(a[0], b[0])));
-        pq.add(new int[] {0, src, k + 1});
-        while (!pq.isEmpty()) {
-            int[] top = pq.remove();
-            int price = top[0];
-            int city = top[1];
-            int stops = top[2];
-            if (city == dst) return price;
-            if (stops > 0) {
-                Map<Integer, Integer> adj = prices.getOrDefault(city, new HashMap<>());
-                for (int a : adj.keySet()) {
-                    pq.add(new int[] {price + adj.get(a), a, stops - 1});
+        
+        public int compareTo(City c) {
+            return this.costFromSrc - c.costFromSrc;
+        }
+    }
+
+    public int findCheapestPriceII(int n, int[][] flights, int src, int dst, int K) {
+    		// Use adjacency matrix to represent city graph
+        int[][] srcToDst = new int[n][n];
+        for (int i = 0; i < flights.length; i++) {
+            srcToDst[flights[i][0]][flights[i][1]] = flights[i][2]; 
+        }
+            
+        PriorityQueue<City> minHeap = new PriorityQueue<>();
+        minHeap.offer(new City(src, 0, 0, 0));
+				
+        int[] cost = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[src] = 0;
+        
+        int[] from = new int[n];
+        Arrays.fill(from, Integer.MAX_VALUE);
+        from[src] = 0;
+        
+        while (!minHeap.isEmpty()) {
+            City curCity = minHeap.poll();
+            
+            from[curCity.id] = curCity.from;
+            
+            if (curCity.id == dst) {
+            		printPath(from, src, dst, curCity.costFromSrc);
+                return curCity.costFromSrc;
+            }
+            
+            if (curCity.stopFromSrc == K + 1) {
+                continue;
+            }
+            
+            int[] nexts = srcToDst[curCity.id];
+            for (int i = 0; i < n; i++) {
+                if (nexts[i] != 0) {
+                    int newCost = curCity.costFromSrc + nexts[i];
+                    int newStop = curCity.stopFromSrc + 1;
+                    if (newCost < cost[i]) {
+                        minHeap.offer(new City(i, newCost, newStop, curCity.id));
+                        cost[i] = newCost;
+                    } else {
+                        minHeap.offer(new City(i, newCost, newStop, curCity.id));
+                    }
                 }
             }
         }
-        return -1;
+       
+        printPath(from, src, dst, cost[dst]);
+        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
     }
 
-
-    
+    private void printPath(int[] from, int src, int dst, int cost) {
+    		System.out.println("II cost = " + cost);
+    		List<Integer> path = new ArrayList<>();
+        
+        int cur = dst;
+        while (cur != src) {
+        		path.add(from[cur]);
+        		cur = from[cur];
+        }
+        for (int i = path.size() - 1; i >= 0; i--) {
+        		System.out.print(path.get(i) + " -> ");
+        }
+        System.out.print(dst);
+        System.out.println();
+    }
 	// Follow up: print path
 
 	/* ----------------------< test stub >-------------------------*/
@@ -130,36 +186,41 @@ public class CheapestFlightsWithinKStops {
 		
 		CheapestFlightsWithinKStops testObj = new CheapestFlightsWithinKStops();
 		
-	/* Test Case 0 */
+		/* Test Case 0 */
 		System.out.println("---< Test Case 0 >---");
 		
-	/* Test Case 1 */
+		/* Test Case 1 */
 		System.out.println("---< Test Case 1 >---");
 		
 		//int n, int[][] flights, int src, int dst, int K
 		int[][] flights = new int[][] {{0,1,100},{1,2,100},{0,2,500}};
 		
-		//int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+		// function signiture: int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
 		testObj.findCheapestPrice(3, flights, 0, 2, 1);
 		
-		System.out.println("II = " + testObj.findCheapestPriceII(3, flights, 0, 2, 1));
+		testObj.findCheapestPriceII(3, flights, 0, 2, 1);
 		
-	/* Test Case 2 */
+		/* Test Case 2 */
 		System.out.println("\n---< Test Case 2 >---");
 
 		// same flights information but requires 0 stops
 		testObj.findCheapestPrice(3, flights, 0, 2, 0);
+		testObj.findCheapestPriceII(3, flights, 0, 2, 0);
 		
-		System.out.println("II = " + testObj.findCheapestPriceII(3, flights, 0, 2, 0));
-		
-	/* Test Case 3 */
+		/* Test Case 3 */
 		System.out.println("\n---< Test Case 3 >---");
-		int[][] flights3 = new int[][] {{0, 1, 1},{1, 2, 1},{2, 3, 1}, 
+		int[][] flights3 = new int[][] {{0,1,5}, {1,2,5}, {0,3,2}, {3,1,2}, {1,4,1}, {4,2,1}};
+		
+		testObj.findCheapestPrice(5, flights3, 0, 2, 2);
+		testObj.findCheapestPriceII(5, flights3, 0, 2, 2);
+		
+		/* Test Case 4 */
+		System.out.println("\n---< Test Case 4 >---");
+		int[][] flights4 = new int[][] {{0, 1, 1},{1, 2, 1},{2, 3, 1}, 
 										{3, 4, 1},{4, 5, 1}, {0, 6, 300},
 										{6, 4, 100}, {0, 7, 350}, {7, 8, 1}, {8, 4, 1}};
 										
-		testObj.findCheapestPrice(9, flights3, 0, 5, 3);
-		
-		System.out.println("II = " + testObj.findCheapestPriceII(9, flights3, 0, 5, 3));
+		testObj.findCheapestPrice(9, flights4, 0, 5, 3);
+		testObj.findCheapestPriceII(9, flights4, 0, 5, 3);
 	}
 }
---
+++ src/heapRelated/DijkstraAlgorithm.java
@@ -0,0 +1,123 @@
+/*
+ * == Created Date ==
+ * Jan 1, 2019
+ * 
+ * == Question - Dijkstra's Algorithm ==
+ * 
+ */
+
+package heapRelated;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+
+public class DijkstraAlgorithm {
+
+    private class Node implements Comparable<Node> {
+        private int id;
+        private int costFromSrc;
+        private int from;
+        public Node(int id, int costFromSrc, int from) {
+            this.id = id;
+            this.costFromSrc = costFromSrc;
+            this.from = from;
+        }
+        
+        public int compareTo(Node c) {
+            return this.costFromSrc - c.costFromSrc;
+        }
+    }
+
+    public int dijkstraAlgorithm(int n, int[][] edges, int src, int dst) {
+    		// Use adjacency matrix to represent city graph
+        int[][] srcToDst = new int[n][n];
+        for (int i = 0; i < edges.length; i++) {
+            srcToDst[edges[i][0]][edges[i][1]] = edges[i][2]; 
+        }
+            
+        PriorityQueue<Node> minHeap = new PriorityQueue<>();
+        minHeap.offer(new Node(src, 0, 0));
+				
+        int[] cost = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[src] = 0;
+        
+        int[] from = new int[n];
+        Arrays.fill(from, Integer.MAX_VALUE);
+        from[src] = 0;
+        
+        Set<Integer> visted = new HashSet<>();
+        
+        while (!minHeap.isEmpty()) {
+        		Node curNode = minHeap.poll();
+        		if (visted.add(curNode.id)) {
+            		from[curNode.id] = curNode.from;
+            		
+                if (curNode.id == dst) {
+                		printPath(from, src, dst);
+                    return curNode.costFromSrc;
+                }
+
+                int[] neighbors = srcToDst[curNode.id];
+                for (int i = 0; i < n; i++) {
+                    if (neighbors[i] != 0) {
+                        int newCost = curNode.costFromSrc + neighbors[i];
+                        if (newCost < cost[i]) {
+                            minHeap.offer(new Node(i, newCost, curNode.id));
+                            cost[i] = newCost;
+                        } 
+                    }
+                }
+        		}
+        }
+       
+        printPath(from, src, dst);
+        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
+    }
+
+    private void printPath(int[] from, int src, int dst) {
+    		List<Integer> path = new ArrayList<>();
+        
+        int cur = dst;
+        while (cur != src) {
+        		path.add(from[cur]);
+        		cur = from[cur];
+        }
+        for (int i = path.size() - 1; i >= 0; i--) {
+        		System.out.print(path.get(i) + " -> ");
+        }
+        System.out.print(dst);
+        System.out.println();
+    }
+    
+	// Time Complexity: O(?);
+	// Space Complexity: O(?);
+	
+	/* ----------------------< test stub >-------------------------*/
+	public static void main(String[] args) {
+		
+		DijkstraAlgorithm testObj = new DijkstraAlgorithm();
+		
+		/* Test Case 0 */
+		System.out.println("---< Test Case 0 >---");
+		
+		/* Test Case 1 */
+		System.out.println("---< Test Case 1 >---");
+		int[][] edges = new int[][] {{0,1,3}, {1,2,5}, {0,3,2}, {3,1,2}, {1,4,1}, {4,2,1}};
+		
+		int cost = testObj.dijkstraAlgorithm(5, edges, 0, 2);
+		System.out.println(cost);
+		
+		/* Test Case 2 */
+		System.out.println("---< Test Case 2 >---");
+		
+		/* Test Case 3 */
+		System.out.println("---< Test Case 3 >---");
+		
+	}
+}
