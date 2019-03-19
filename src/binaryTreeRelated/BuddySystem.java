ce455101a4c0095e47cf32386990f5261d16b196
bchen22@syr.edu <bchen22@syr.edu>
2019-03-19 16:12:17 +0800

    practice

--- src/binaryTreeRelated/BuddySystem.java
+++ src/binaryTreeRelated/BuddySystem.java
@@ -72,5 +72,22 @@ public class BuddySystem {
 		
 	}
 	
-	
+	/* ----------------------< test stub >-------------------------*/
+	public static void main(String[] args) {
+		
+		// Class testObj = new Class();
+		
+		/* Test Case 0 */
+		System.out.println("---< Test Case 0 >---");
+		
+		/* Test Case 1 */
+		System.out.println("---< Test Case 1 >---");
+		
+		/* Test Case 2 */
+		System.out.println("---< Test Case 2 >---");
+		
+		/* Test Case 3 */
+		System.out.println("---< Test Case 3 >---");
+		
+	}
 }
